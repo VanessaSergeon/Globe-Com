d62b5526e86552ac3d10ebdfba79f91ab85314f4
Vanessa Sergeon <vsergeon@gmail.com>
2014-08-13 15:49:48 -0400

    Add jquery click to add schedule table row

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,18 +1,39 @@
-// ****** PRICING TAB *******
-
-// New pricing click function
 $(document).ready(function() {
 
-  var numPricings = 0;
-  $('#newPrice').click(function() {
-    numPricings++;
-    var pricing = '<form class="form-inline">';
+// ****************** TRADE PAGE *********************
+
+// Trade panel
+
+// New pricing click function
+
+var numPricings = 0;
+$('#newPrice').click(function() {
+  numPricings++;
+  var pricing = '<form class="form-inline">';
     pricing += '<label>Pricing '+numPricings+'</label>';
     pricing += '<input class="form-control" type="number">';
     pricing += '  @  <input class="form-control" type="text" placeholder="00.00 cts">';
     pricing += '  <input class="form-control" type="text" placeholder="Date">';
     pricing += '</form>';
-    $('#prices').append(pricing);
-  });
-
+  $('#prices').append(pricing);
 });
+
+// New shipment click function
+
+$('#newShipment').click(function() {
+  var shipment ='<tr>';
+    shipment+= '<td><input class="form-control" type="text"></td>';
+    shipment+= '<td><input class="form-control" type="text"></td>';
+    shipment+= '<td><input class="form-control" type="text"></td>';
+    shipment+= '<td><input class="form-control" type="text"></td>';
+    shipment+= '<td><input class="form-control" type="text"></td>';
+    shipment+= '</tr>';
+  $('#scheduleTable').append(shipment);
+});
+
+
+
+
+
+}); // ************** END DOCUMENT READY ****************
+
